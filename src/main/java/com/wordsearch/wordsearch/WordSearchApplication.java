86029df00034efaa198940e4a3462658661fe5f6
Harshvardhan Khachane <hvkhachane8@gmail.com>
2025-04-06 14:55:23 +0530

    Added function to load words from words.txt

--- src/main/java/com/wordsearch/wordsearch/WordSearchApplication.java
+++ src/main/java/com/wordsearch/wordsearch/WordSearchApplication.java
@@ -1,13 +1,34 @@
 package com.wordsearch.wordsearch;
 
+import com.wordsearch.wordsearch.service.WordService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+
+import java.nio.file.Files;
+import java.util.List;
 
 @SpringBootApplication
-public class WordSearchApplication {
+public class WordSearchApplication implements CommandLineRunner {
+    private final WordService wordService;
+
+    public WordSearchApplication(WordService wordService) {
+        this.wordService = wordService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WordSearchApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        ClassPathResource resource = new ClassPathResource("words.txt");
+        List<String> words = Files.readAllLines(resource.getFile().toPath());
+        wordService.loadWords(words);
+        System.out.println("Loaded " + words.size() + " words");
+
     }
 
 }
---
+++ src/main/java/com/wordsearch/wordsearch/service/WordService.java
@@ -0,0 +1,25 @@
+package com.wordsearch.wordsearch.service;
+
+import com.wordsearch.wordsearch.model.Trie;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class WordService {
+    private final Trie trie = new Trie();
+    private final Map<String, Integer> wordRanks = new ConcurrentHashMap<>();
+
+    // Function to load words from a list(To be called at startup)
+    public void loadWords(List<String> words) {
+        Set<String> uniqueWords = new HashSet<>(words);
+        uniqueWords.forEach(word -> {
+            trie.insert(word);
+            wordRanks.put(word, 0);
+        });
+    }
+}
